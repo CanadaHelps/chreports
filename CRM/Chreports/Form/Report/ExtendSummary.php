14d0bc3573d1b38ba1278619da4d1b44b3772287
Monish Deb <deb.monish@gmail.com>
2019-10-31 00:05:52 +0530

    minor fix

--- CRM/Chreports/Form/Report/ExtendSummary.php
+++ CRM/Chreports/Form/Report/ExtendSummary.php
@@ -5,11 +5,13 @@ class CRM_Chreports_Form_Report_ExtendSummary extends CRM_Report_Form_Contribute
 
   public function from($entity = NULL) {
     parent::from($entity);
-    $this->_from .= "
-    LEFT JOIN civicrm_line_item li ON li.contribution_id = contribution_civireport.id
-    LEFT JOIN civicrm_financial_item fi ON fi.entity_id = li.id AND fi.entity_table = 'civicrm_line_item'
-    LEFT JOIN civicrm_financial_account fa ON fa.id = fi.financial_account_id
-    ";
+    if (!strstr($this->_from, 'civicrm_line_item li')) {
+      $this->_from .= "
+      LEFT JOIN civicrm_line_item li ON li.contribution_id = contribution_civireport.id
+      LEFT JOIN civicrm_financial_item fi ON fi.entity_id = li.id AND fi.entity_table = 'civicrm_line_item'
+      LEFT JOIN civicrm_financial_account fa ON fa.id = fi.financial_account_id
+      ";
+    }
   }
 
 }
