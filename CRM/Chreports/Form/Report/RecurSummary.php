85770438534997438f5cbd523f9971a54842a8a4
Monish Deb <deb.monish@gmail.com>
2020-04-15 16:14:36 +0530

    fixed order of columns

--- CRM/Chreports/Form/Report/RecurSummary.php
+++ CRM/Chreports/Form/Report/RecurSummary.php
@@ -156,18 +156,18 @@ class CRM_Chreports_Form_Report_RecurSummary extends CRM_Report_Form {
 
     $columnHeaders = [];
     foreach ([
-      'civicrm_contact_id', 
+      'civicrm_contact_exposed_id',
       'civicrm_contact_sort_name',
       'civicrm_address_street_address',
       'civicrm_address_city',
       'civicrm_address_state_province_id',
       'civicrm_address_postal_code',
       'civicrm_address_country_id',
-      'civicrm_email_email',
       'civicrm_phone_phone',
+      'civicrm_email_email',
       'civicrm_contribution_source',
-      'civicrm_contribution_total_amount',
       'civicrm_contribution_last_month_amount',
+      'civicrm_contribution_total_amount',
       'civicrm_contribution_start_date',
       'civicrm_contribution_completed_contributions',
     ] as $name) {
