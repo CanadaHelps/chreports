a5149a6b56fb9d94f630a9c6b58c051133ba8296
Monish Deb <deb.monish@gmail.com>
2020-04-21 13:41:19 +0530

    corrent the mgd file of extended-summary page

--- CRM/Chreports/Form/Report/ExtendSummary.mgd.php
+++ CRM/Chreports/Form/Report/ExtendSummary.mgd.php
@@ -13,8 +13,8 @@ return array (
     'params' =>
     array (
       'version' => 3,
-      'label' => 'Recurring Contributions (Summary)',
-      'description' => 'Overview of Recurring Contributions (biz.jmaconsulting.chreports)',
+      'label' => 'Extended Contribution Summary',
+      'description' => 'Extended Contributions Summary (biz.jmaconsulting.chreports)',
       'class_name' => 'CRM_Chreports_Form_Report_ExtendSummary',
       'report_url' => 'biz.jmaconsulting.chreports/extendsummary',
       'component' => '',
