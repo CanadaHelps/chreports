245bc9b1060274a3821f142f92ee120bc07855fe
Irfan Haque <irfanh@canadahelps.org>
2020-07-17 11:01:36 -0400

    Added Contact Detailed Report

---
+++ managed/ContactReportDetailed.mgd.php
@@ -0,0 +1,20 @@
+<?php
+return array(
+  array(
+    'module' => 'biz.jmaconsulting.chreports',
+    'name' => 'Contact Report (Detailed)',
+    'update' => 'never',
+    'entity' => 'ReportInstance',
+    'params' => array(
+      'version' => 3,
+      'report_id' => 'contact/summary',
+      'name' => 'Contact Report (Detailed)',
+      'title' => ts('Contact Report (Detailed)'),
+      "description" => "Provides a list of contact records in your system",
+      'permission' => 'access CiviReport',
+      'is_active' => 1,
+      "form_values" => 'a:85:{s:8:"entryURL";s:72:"https://test2-dms.canadahelps.org/civicrm/report/contact/summary?reset=1";s:6:"fields";a:12:{s:9:"sort_name";s:1:"1";s:10:"exposed_id";s:1:"1";s:5:"email";s:1:"1";s:5:"phone";s:1:"1";s:9:"phone_ext";s:1:"1";s:22:"address_street_address";s:1:"1";s:30:"address_supplemental_address_1";s:1:"1";s:30:"address_supplemental_address_2";s:1:"1";s:12:"address_city";s:1:"1";s:19:"address_postal_code";s:1:"1";s:25:"address_state_province_id";s:1:"1";s:18:"address_country_id";s:1:"1";}s:12:"sort_name_op";s:3:"has";s:15:"sort_name_value";s:0:"";s:9:"source_op";s:3:"has";s:12:"source_value";s:0:"";s:6:"id_min";s:0:"";s:6:"id_max";s:0:"";s:5:"id_op";s:3:"lte";s:8:"id_value";s:0:"";s:12:"gender_id_op";s:2:"in";s:15:"gender_id_value";a:0:{}s:19:"birth_date_relative";s:0:"";s:15:"birth_date_from";s:0:"";s:13:"birth_date_to";s:0:"";s:15:"contact_type_op";s:2:"in";s:18:"contact_type_value";a:0:{}s:19:"contact_sub_type_op";s:2:"in";s:22:"contact_sub_type_value";a:0:{}s:22:"modified_date_relative";s:0:"";s:18:"modified_date_from";s:0:"";s:16:"modified_date_to";s:0:"";s:14:"is_deceased_op";s:2:"eq";s:17:"is_deceased_value";s:1:"0";s:15:"do_not_email_op";s:2:"eq";s:18:"do_not_email_value";s:0:"";s:15:"do_not_phone_op";s:2:"eq";s:18:"do_not_phone_value";s:0:"";s:14:"do_not_mail_op";s:2:"eq";s:17:"do_not_mail_value";s:0:"";s:13:"is_opt_out_op";s:2:"eq";s:16:"is_opt_out_value";s:0:"";s:21:"preferred_language_op";s:2:"in";s:24:"preferred_language_value";a:0:{}s:13:"is_deleted_op";s:2:"eq";s:16:"is_deleted_value";s:1:"0";s:25:"address_street_address_op";s:3:"has";s:28:"address_street_address_value";s:0:"";s:15:"address_city_op";s:3:"has";s:18:"address_city_value";s:0:"";s:22:"address_postal_code_op";s:3:"has";s:25:"address_postal_code_value";s:0:"";s:28:"address_state_province_id_op";s:2:"in";s:31:"address_state_province_id_value";a:0:{}s:21:"address_country_id_op";s:2:"in";s:24:"address_country_id_value";a:0:{}s:8:"tagid_op";s:2:"in";s:11:"tagid_value";a:0:{}s:6:"gid_op";s:2:"in";s:9:"gid_value";a:0:{}s:13:"custom_14_min";s:0:"";s:13:"custom_14_max";s:0:"";s:12:"custom_14_op";s:3:"lte";s:15:"custom_14_value";s:0:"";s:13:"custom_15_min";s:0:"";s:13:"custom_15_max";s:0:"";s:12:"custom_15_op";s:3:"lte";s:15:"custom_15_value";s:0:"";s:18:"custom_16_relative";s:0:"";s:14:"custom_16_from";s:0:"";s:12:"custom_16_to";s:0:"";s:18:"custom_17_relative";s:0:"";s:14:"custom_17_from";s:0:"";s:12:"custom_17_to";s:0:"";s:13:"custom_18_min";s:0:"";s:13:"custom_18_max";s:0:"";s:12:"custom_18_op";s:3:"lte";s:15:"custom_18_value";s:0:"";s:13:"custom_19_min";s:0:"";s:13:"custom_19_max";s:0:"";s:12:"custom_19_op";s:3:"lte";s:15:"custom_19_value";s:0:"";s:9:"order_bys";a:1:{i:1;a:2:{s:6:"column";s:9:"sort_name";s:5:"order";s:3:"ASC";}}s:11:"description";s:49:"Provides a list of contact records in your system";s:13:"email_subject";s:0:"";s:8:"email_to";s:0:"";s:8:"email_cc";s:0:"";s:9:"row_count";s:0:"";s:9:"view_mode";s:8:"criteria";s:13:"cache_minutes";s:2:"60";s:10:"permission";s:17:"access CiviReport";s:9:"parent_id";s:0:"";s:8:"radio_ts";s:0:"";s:6:"groups";s:0:"";s:11:"instance_id";s:2:"30";}',
+      'is_reserved' =>  0,
+    ),
+  ),
+);
