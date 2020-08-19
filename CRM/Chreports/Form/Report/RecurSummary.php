98f38109086149c611287a1dca53d3791df307e7
Edsel Lopez <edsel.lopez@jmaconsulting.biz>
2020-08-19 13:16:59 +0530

    Fixed statistics query

--- CRM/Chreports/Form/Report/RecurSummary.php
+++ CRM/Chreports/Form/Report/RecurSummary.php
@@ -158,11 +158,13 @@ class CRM_Chreports_Form_Report_RecurSummary extends CRM_Report_Form {
 
   public function statistics(&$rows) {
     $selectColumns = [
-      "SUM(" . $this->_columns['civicrm_contribution']['fields']['total_amount']['dbAlias'] . ") as `civicrm_contribution_total_amount`",
-      "SUM(" . $this->_columns['civicrm_contribution']['fields']['last_month_amount']['dbAlias'] . ") as `civicrm_contribution_last_month_amount`",
+      $this->_columns['civicrm_contribution']['fields']['total_amount']['dbAlias'] . " as `civicrm_contribution_total_amount`",
+      $this->_columns['civicrm_contribution']['fields']['last_month_amount']['dbAlias'] . " as `civicrm_contribution_last_month_amount`",
     ];
 
-    $sql = "SELECT " . implode(", ", $selectColumns) . " $this->_from $this->_where ";
+    $sql = "SELECT SUM(stat.civicrm_contribution_total_amount) AS civicrm_contribution_total_amount,
+            SUM(stat.civicrm_contribution_last_month_amount) AS civicrm_contribution_last_month_amount
+            FROM (SELECT " . implode(", ", $selectColumns) . " $this->_from $this->_where $this->_groupBy) AS stat";
     $this->addToDeveloperTab($sql);
     $results = CRM_Core_DAO::executeQuery($sql)->fetchAll();
 
